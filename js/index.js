c1caaa98dea611cd168447830fa4d7b76f4e1e44
victorteje <vteje2@gmail.com>
2023-06-02 22:20:50 +0200

    poor man's version done

--- js/index.js
+++ js/index.js
@@ -1,4 +1,21 @@
 const headsOrTails = () => {
-    let flipCoin = document.getElementById("heads");
-    
+    let result = Math.round(Math.random());
+    if (result === 1)
+    {
+        let headsWins = document.getElementById("tails");
+        headsWins.style.opacity = 0;
+    }
+
+    else
+    {
+        let tailsWins = document.getElementById("heads");
+        tailsWins.style.opacity = 0;  
+    }
+}
+
+const resetGame = () => {
+    let resetHeads = document.getElementById("heads");
+    resetHeads.style.opacity = 1;
+    let resetTails = document.getElementById("tails");
+    resetTails.style.opacity = 1;
 }
