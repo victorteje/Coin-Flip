9e0bdfda2c8c9d9e4abfd521fefe6abf537f203d
victorteje <vteje2@gmail.com>
2023-06-03 14:53:59 +0200

    working on animation

--- js/index.js
+++ js/index.js
@@ -5,17 +5,23 @@ const headsOrTails = () => {
         let headsWins = document.getElementById("heads");
         headsWins.style.opacity = 1;
     }
-
     else
     {
         let tailsWins = document.getElementById("tails");
-        tailsWins.style.opacity = 1;  
+        tailsWins.style.opacity = 1;
     }
+}
+
+
+document.addEventListener("click", spinCoin);
+function spinCoin() {
+    let flip = document.getElementById("coin");
+    flip.classList.add("spinAnim");
 }
 
 const resetGame = () => {
     let resetHeads = document.getElementById("heads");
-    resetHeads.style.opacity = 0;
+    resetHeads.style.opacity = 1;
     let resetTails = document.getElementById("tails");
     resetTails.style.opacity = 0;
 }
