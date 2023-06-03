bc19cf65e7795936f513b0d7d7e3a683ce528eae
victorteje <vteje2@gmail.com>
2023-06-03 19:14:20 +0200

    working on spin anim 2

--- js/index.js
+++ js/index.js
@@ -1,20 +1,29 @@
 const headsOrTails = () => {
-    let result = Math.round(Math.random());
-    if (result === 1)
-    {
-        let headsWins = document.getElementById("heads");
-        headsWins.style.opacity = 1;
-    }
-    else
-    {
-        let tailsWins = document.getElementById("tails");
-        tailsWins.style.opacity = 1;
-    }
+    // document.addEventListener("click", resetTails);
+    // function resetTails() {
+    //     let hideTails = document.getElementById("tails");
+    //      hideTails.style.opacity = 0;
+    //     }
     document.addEventListener("click", spinCoin);
     function spinCoin() {
         let flip = document.getElementById("coin");
         flip.removeAttribute("class");
         flip.classList.add("spinAnim");
+    }
+    let result = Math.round(Math.random());
+    if (result === 1)
+    {   
+        let tailsLoses = document.getElementById("tails");
+        tailsLoses.style.opacity = 0;
+        let headsWins = document.getElementById("heads");
+        headsWins.style.opacity = 1;
+    }
+    else
+    {
+        let headsLoses = document.getElementById("heads");
+        headsLoses.style.opacity = 0;
+        let tailsWins = document.getElementById("tails");
+        tailsWins.style.opacity = 1;
     }
 }
 
