0124ad84e42aa3503f1c6a6dd1f9751c23aa28db
victorteje <vteje2@gmail.com>
2023-06-03 11:47:25 +0200

    adjusted elements' positions

--- js/index.js
+++ js/index.js
@@ -2,20 +2,20 @@ const headsOrTails = () => {
     let result = Math.round(Math.random());
     if (result === 1)
     {
-        let headsWins = document.getElementById("tails");
-        headsWins.style.opacity = 0;
+        let headsWins = document.getElementById("heads");
+        headsWins.style.opacity = 1;
     }
 
     else
     {
-        let tailsWins = document.getElementById("heads");
-        tailsWins.style.opacity = 0;  
+        let tailsWins = document.getElementById("tails");
+        tailsWins.style.opacity = 1;  
     }
 }
 
 const resetGame = () => {
     let resetHeads = document.getElementById("heads");
-    resetHeads.style.opacity = 1;
+    resetHeads.style.opacity = 0;
     let resetTails = document.getElementById("tails");
-    resetTails.style.opacity = 1;
+    resetTails.style.opacity = 0;
 }
